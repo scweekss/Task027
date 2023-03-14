41e7511512ec8794d464593d74b5a52c8542c10b
scweekkkkk <124443824+scweekss@users.noreply.github.com>
2023-03-14 17:59:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число ");
+
+        int number = Convert.ToInt32(Console.ReadLine()!);
+
+        int sum(int number)
+
+        {
+            int count = Convert.ToString(number).Length;
+            int a = 0;
+            int result = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                a = number - number % 10;
+                result = result + (number - a);
+                number = number / 10;
+            }
+            return result;
+        }
+
+        int sumNumber = sum(number);
+        Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+    }
+}
